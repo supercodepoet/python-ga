ea3ff6d3ba0a5185cdb7a39f4cdf92478662eeb8
Travis Chase <travis@travischase.me>
2012-01-25 13:33:57 -0600

    Initial creation

---
+++ README.rst
@@ -0,0 +1,21 @@
+python-ga
+==========================
+
+Implementation of a generic server-side Google Analytics client in Python
+that implements nearly every parameter and tracking feature of the original
+GA Javascript client.
+
+This is a port of php-ga: http://code.google.com/p/php-ga/
+
+Credits
+-------
+
+- `php-ga`_
+- `Distribute`_
+- `Buildout`_
+- `modern-package-template`_
+
+.. _php-ga: http://code.google.com/p/php-ga/
+.. _Buildout: http://www.buildout.org/
+.. _Distribute: http://pypi.python.org/pypi/distribute
+.. _`modern-package-template`: http://pypi.python.org/pypi/modern-package-template
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='python-ga',
+    version=version,
+    description="Implementation of a generic server-side Google Analytics client in Python",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='google analytics',
+    author='Travis Chase',
+    author_email='travis@travischase.me',
+    url='',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['python-ga=pythonga:main']
+    }
+)
